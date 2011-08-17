dc9fa2b655bb982d30090157c0fd577b79bc0c16
Andrew Kane <andrew@getformidable.com>
2011-08-17 00:10:26 -0700

    Use em-http-request

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source "http://rubygems.org"
 
-gem "yajl-ruby", :git => "git@github.com:ankane/yajl-ruby.git"
 gem "data_mapper"
 gem "dm-mysql-adapter"
+gem "em-http-request", :git => "git://github.com/ankane/em-http-request.git"
 gem "simple_oauth"
+gem "yajl-ruby"
--- stream.rb
+++ stream.rb
@@ -1,16 +1,10 @@
-require "yajl/http_stream"
+require "em-http"
+require "em-http/middleware/oauth"
 require "simple_oauth"
+require "yajl"
 
 require_relative "models"
 
-url = "http://stream.twitter.com/1/statuses/filter.json"
-params = {:track => "i want"}
-
-# Read OAuth config file and symbolize keys.
-oauth = YAML.load_file("config/oauth.yml").inject({}){|memo,(k,v)| memo[k.to_sym] = v; memo}
-oauth_header = SimpleOAuth::Header.new("POST", url, params, oauth)
-
-words = {}
 captured = 0
 
 trap("INT") do
@@ -19,14 +13,28 @@ trap("INT") do
   exit(0)
 end
 
-Yajl::HttpStream.post(url, params, {:symbolize_keys => true, :headers => {"Authorization" => oauth_header}}) do |hash|
-  next if hash[:delete]
+parser = Yajl::Parser.new(:symbolize_keys => true)
+parser.on_parse_complete = lambda do |tweet|
+  next if tweet[:delete]
 
   begin
-    Post.create!(:id => hash[:id], :user => hash[:user][:screen_name], :body => hash[:text], :created_at => hash[:created_at])
+    Post.create!(:id => tweet[:id], :user => tweet[:user][:screen_name], :body => tweet[:text], :created_at => tweet[:created_at])
     putc "."
-    captured += 1
   rescue DataObjects::IntegrityError => e
     putc "X"
+  end
+  captured += 1
+end
+
+EM.run do
+  conn = EventMachine::HttpRequest.new("http://stream.twitter.com/1/statuses/filter.json")
+
+  # sign the request with OAuth credentials
+  oauth_opts = YAML.load_file("config/oauth.yml").inject({}){|memo,(k,v)| memo[k.to_sym] = v; memo}
+  conn.use EventMachine::Middleware::OAuth, oauth_opts
+
+  http = conn.post :body => {:track => "i want"}
+  http.stream do |chunk|
+    parser << chunk
   end
 end
